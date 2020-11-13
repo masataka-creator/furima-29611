8e38602b58b06ed8c129190bbc72a3e0df4d0531
masataka-creator <masataka.asano.0101@gmail.com>
2020-11-13 09:46:47 +0900

    ログイン画面へ遷移できるリンク実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ class ApplicationController < ActionController::Base
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :encrypted_password, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthdate])
   end
 
   def basic_auth
