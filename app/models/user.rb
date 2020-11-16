1125800120c3caf13e976699e4c6fe6ea66dd0ea
masataka-creator <masataka.asano.0101@gmail.com>
2020-11-16 20:37:03 +0900

    ユーザー新規登録のテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, :email, presence: true
-         validates :encrypted_password, presence: true
-         validates :first_name, :last_name, presence: true
-         validates :first_name_kana, :last_name_kana, presence: true
+         validates :nickname, presence: true
+         validates :password, presence: true, length: { minimum: 6}, format: {with: /\A[a-zA-Z0-9]+\z/}
+         # 漢字と全角の平仮名とカタカナのバリデーション
+         validates :first_name, :last_name, presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
+         # 全角カタカナのバリデーション
+         validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
+         validates :birthdate, presence: true
         end
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,34 +1,111 @@
 require 'rails_helper'
-describe User do
+
+RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録がうまくいくとき' do
-      it "nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる" do
-      end
-      it "nicknameが6文字以下で登録できる" do
-      end
-      it "passwordが6文字以上であれば登録できる" do
-      end
+    it "ニックネームが空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.nickname = ""  # nicknameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Nickname can't be blank")
     end
 
-    context '新規登録がうまくいかないとき' do
-      it "nicknameが空だと登録できない" do
-      end
-      it "nicknameが7文字以上であれば登録できない" do
-      end
-      it "emailが空では登録できない" do
-      end
-      it "重複したemailが存在する場合登録できない" do
-      end
-      it "passwordが空では登録できない" do
-      end
-      it "passwordが5文字以下であれば登録できない" do
-      end
-      it "passwordが存在してもpassword_confirmationが空では登録できない" do
-      end
+    it "メールアドレスが空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.email = ""  # emailの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Email can't be blank")
+    end
+
+    it "パスワードが空だと登録できない" do
+      user = FactoryBot.build(:user)  # passwordのインスタンス生成
+      user.password = ""  # passwordの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "パスワードと確認パスワードが一致していないと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.password = "aaaaaa"  # password_confirmationの値をpasswordの値と違う値にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+    it "性が空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.first_name = ""  # first_nameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("First name can't be blank")
+    end
+
+    it "名が空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.last_name = ""  # last_nameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name can't be blank")
+    end
+
+    it "性カナが空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.first_name_kana = ""  # first_nameの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("First name kana can't be blank")
+    end
+
+    it "名カナが空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.last_name_kana = ""  # last_name_kanaの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name kana can't be blank")
+    end
+
+    it "パスワードが6文字未満だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.password = ""  # passwordの値を6文字未満にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+
+    it "生年月日が空だと登録できない" do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.birthdate = ""  # birthdateの値を空にする
+      user.valid?
+      expect(user.errors.full_messages).to include("Birthdate can't be blank")
+    end
+  end
+
+  describe '名前を半角で入力' do
+    it '性を半角で入力した時登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.first_name_kana = "ﾔﾏﾀﾞ"  # first_name_kanaの値を半角にする
+      user.valid?
+      expect(user.errors.full_messages).to include("First name kana is invalid")
+    end
+ 
+    it '名を半角で入力した時登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.last_name_kana = "ﾘｸﾀﾛｳ"  # last_name_kanaの値を半角にする
+      binding.pry
+      expect(user.errors.full_messages).to include("Last name kana is invalid")
+    end
+  end
+
+  describe 'フリガナをカタカナ意外で入力' do
+    it '性カナをカタカナ意外で入力した時登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.first_name_kana = ""  # first_name_kanaの値を全角片仮名以外にする
+      user.valid?
+      expect(user.errors.full_messages).to include("First name kana is invalid")
+    end
+ 
+    it '名カナをカタカナ意外で入力した時登録できない' do
+      user = FactoryBot.build(:user)  # Userのインスタンス生成
+      user.last_name_kana = ""  # last_name_kanaの値を全角片仮名以外にする      
+      user.valid?
+      expect(user.errors.full_messages).to include("Last name kana is invalid")
     end
   end
 end
