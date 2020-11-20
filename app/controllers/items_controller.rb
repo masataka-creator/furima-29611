4e1b135bd3fb7ff701e759635a20e9dd185778ee
masataka-creator <masataka.asano.0101@gmail.com>
2020-11-20 12:37:39 +0900

    カメラボタンで出品ページへ移動

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,10 @@ class ItemsController < ApplicationController
     @item = 'これはコントローラーで定義したインスタンス変数を確認するための文字列です'
   end
 
+  def def new
+    @item = Item.new
+  end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
