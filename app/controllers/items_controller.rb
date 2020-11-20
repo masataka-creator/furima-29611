976f8d4f809a563e5bc702550a89b2c6e0f195c3
masataka-creator <masataka.asano.0101@gmail.com>
2020-11-20 10:52:31 +0900

    ActiveStorage導入#2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :set_item, only: [:edit, :update]
 
   def index  # indexアクションを定義した
     @item = Item.all
@@ -7,6 +8,14 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new  # 商品ページの新規作成なので「new」アクションを定義
+  end
+
+  def edit
+  end
+
+  def update
+    @item.update(item_params)
+    redirect_to root_path
   end
 
   def create
@@ -19,7 +28,11 @@ class ItemsController < ApplicationController
   end
 
   def item_params
-    params.require(:message).permit(:content, :image)
+    params.require(:item).permit(:item_images, :name, :description, :category_id, :condition_id, :delivery_charge_id, :delivery_area_id, :delivery_days_id, :price, :user)
+  end
+
+  def set_message
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
  devise_for :users
  root "items#index"
  resources :users, only: [:edit, :update]
- resources :items, only: [:new, :create]
+ resources :items, only: [:new, :create, :edit, :update]
  
 end
